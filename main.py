99f7924c6eaa96889ed6f028bab6dc3606ea2877
Che <53223732+che-a@users.noreply.github.com>
2022-05-19 22:53:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+Скрипт преобразования данных их Excel таблицы в JSON-файл.
+"""
+import json
+from pprint import pprint
+
+from openpyxl import load_workbook
+
+
+def load_limits_file(filename='limits.json'):
+    """
+    Загрузка файла ограничений столбцов и строк
+    """
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def load_excel_file(filename='input.xlsx'):
+    """
+    Загрузка данных из Excel-файла
+    """
+    limits = load_limits_file()
+    wb = load_workbook(filename)
+    ws = wb[limits['sheet']]
+
+    data = {}
+    for row in range(limits['begin_row'], limits['end_row'] + 1):
+        if row not in limits['exclude_rows']:
+            unique_key = ws[limits['unique_key_column'] + str(row)].value
+            data[unique_key] = {}
+
+            for column_name, column_value in limits['columns'].items():
+                if column_value != limits['unique_key_column']:
+                    data[unique_key][column_name] = ws[column_value + str(row)].value
+
+    return data
+
+
+if __name__ == '__main__':
+    pprint(load_excel_file())
