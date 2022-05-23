1e7dd28243ade9bc8825889b1d744271bff952d9
Che <53223732+che-a@users.noreply.github.com>
2022-05-23 17:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,64 @@
-!#/usr/bin/env python3
-
 """
-Скрипт преобразования всех или части данных из Excel-таблицы в JSON-файл.
+Скрипт преобразования данных из файла Excel в файл JSON.
 """
 import json
-from pprint import pprint
 
 from openpyxl import load_workbook
 
+FILE_LIMITS = "limits.json"
+FILE_INPUT = "input.xlsx"
+FILE_OUTPUT = "output.json"
+ERROR_NOT_FOUND_LIMITS_FILE = 2
+ERROR_NOT_FOUND_INPUT_FILE = 3
 
-def load_limits_file(filename='limits.json'):
-    """
-    Загрузка файла ограничений столбцов и строк
-    """
+
+def load_limits_file(filename=FILE_LIMITS):
+    """Загрузка JSON-файла с настройками ограничений."""
     with open(filename) as json_file:
-        data = json.load(json_file)
-    return data
+        result = json.load(json_file)
+    return result
 
 
-def load_excel_file(filename='input.xlsx'):
+def load_excel_file(limits_dict, filename=FILE_INPUT):
     """
-    Загрузка данных из Excel-файла
+    Загрузка данных из Excel-файла.
     """
-    limits = load_limits_file()
     wb = load_workbook(filename)
-    ws = wb[limits['sheet']]
+    ws = wb[limits_dict['sheet']]
 
-    data = {}
-    for row in range(limits['begin_row'], limits['end_row'] + 1):
-        if row not in limits['exclude_rows']:
-            unique_key = ws[limits['unique_key_column'] + str(row)].value
-            data[unique_key] = {}
+    result = {}
+    for row in range(limits_dict['begin_row'], limits_dict['end_row'] + 1):
+        if row not in limits_dict['exclude_rows']:
+            unique_key = str(ws[limits_dict['unique_key_column'] + str(row)].value)
+            result[unique_key] = {}
 
-            for column_name, column_value in limits['columns'].items():
-                if column_value != limits['unique_key_column']:
-                    data[unique_key][column_name] = ws[column_value + str(row)].value
+            for column_name, column_value in limits_dict['columns'].items():
+                if column_value != limits_dict['unique_key_column']:
+                    result[unique_key][column_name] = ws[column_value + str(row)].value
+    return result
 
-    return data
+
+def save_json_file(data_dict, filename=FILE_OUTPUT):
+    with open(filename, 'w') as outfile:
+        json.dump(data_dict, outfile, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pprint(load_excel_file())
+    # Чтение JSON-файла с настройками ограничений.
+    limits = dict()
+    try:
+        limits = load_limits_file()
+    except FileNotFoundError:
+        print(f"Ошибка: не найден файл {FILE_LIMITS}")
+        exit(ERROR_NOT_FOUND_LIMITS_FILE)
+
+    # Чтение входного Excel-файла.
+    data = dict()
+    try:
+        data = load_excel_file(limits)
+    except FileNotFoundError:
+        print(f"Ошибка: не найден файл {FILE_INPUT}")
+        exit(ERROR_NOT_FOUND_INPUT_FILE)
+
+    # Сохранение JSON-файла
+    save_json_file(data)
